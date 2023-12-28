cb19227a55fb1b527147ecefb04df55ef28cbfb5
Juan Narvaez <juand.ns07@gmail.com>
2023-12-28 16:30:16 -0300

    ✨[ADD] Update titles and add Contact component

--- src/site.config.ts
+++ src/site.config.ts
@@ -4,7 +4,7 @@ export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
 	author: "Chris Williams",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Astro Theme Cactus",
+	title: "Portfolio de Juan Narvaez",
 	// Meta property used as the default description meta property
 	description: "An opinionated starter theme for Astro",
 	// HTML lang property, found in src/layouts/Base.astro L:18
@@ -34,6 +34,10 @@ export const menuLinks: Array<{ title: string; path: string }> = [
 	{
 		title: "Proyectos",
 		path: "#projects",
+	},
+	{
+		title: "Contacto",
+		path: "#contact",
 	},
 	{
 		title: "Blog",
