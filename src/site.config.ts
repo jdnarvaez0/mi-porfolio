95b23344c775214db8239e0c6607b2af5f03df59
Juan Narvaez <juand.ns07@gmail.com>
2023-12-28 15:45:06 -0300

    ✨ [ADD] stack&tools section and update timeLine

--- src/site.config.ts
+++ src/site.config.ts
@@ -28,12 +28,12 @@ export const siteConfig: SiteConfig = {
 // Used to generate links in both the Header & Footer.
 export const menuLinks: Array<{ title: string; path: string }> = [
 	{
-		title: "Home",
-		path: "/",
+		title: "Experiencia",
+		path: "#experience",
 	},
 	{
-		title: "About",
-		path: "/about/",
+		title: "Proyectos",
+		path: "#projects",
 	},
 	{
 		title: "Blog",
